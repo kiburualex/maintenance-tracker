09b645fe64e16cfb63eee2dc3096dbcfcc5006d1
desshub <desshub95@gmail.com>
2018-05-29 16:31:26 +0300

    [Chore #157932103] Test To Test Successfull Registration

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
 #!flask/bin/python
-from flask import Flask
+import os
 
-app = Flask(__name__)
+from app import create_app
 
-@app.route('/')
-def index():
-    return "Hello "
+config_name = os.getenv('FLASK_CONFIG')
+app = create_app(config_name)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ tests/test_user.py
@@ -0,0 +1,35 @@
+"""This module defines tests for the user class and its methods"""
+import pytest
+import os
+
+from flask_testing import TestCase
+
+from app import create_app
+from app.user import User_details
+
+class TestBase(TestCase):
+
+    def create_app(self):
+
+        # pass in test configurations
+        config_name = 'testing'
+        app = create_app(config_name)
+        return app
+
+class UserTests(TestCase):
+    """Define and setup testing class"""
+
+    def setUp(self):
+        """ Set up user object before each test"""
+        self.user = User_details()
+
+    def tearDown(self):
+        """ Clear up objects after every test"""
+        del self.user
+
+    @pytest.fixture(scope='module')
+    def test_isuccessful_registration(self):
+        """Test is a user with correct credentials can register sucessfully"""
+        res = self.user.register("desmond", "desmond@mail.com", "pass1234", "pass1234")
+        self.assertEqual(res, "Registration successfull")
+    
