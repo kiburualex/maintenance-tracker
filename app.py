b08ff802f9a0ac4f6940ce0b03ba36a5ede4a100
desshub <desshub95@gmail.com>
2018-05-31 01:19:04 +0300

    [Chore] Indentation Issue

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ config_name = os.getenv('FLASK_CONFIG')
 app = create_app(config_name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	port = int(os.environ.get('PORT', 5000))
+	app.run('', port=port)
