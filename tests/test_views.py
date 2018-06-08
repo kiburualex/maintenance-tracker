419182b49298074b95daddd44a5018edae155e28
desshub <desshub95@gmail.com>
2018-06-08 12:32:44 +0300

    [Chore #158210822] Write Tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -17,13 +17,6 @@ class TestBase(TestCase):
 
 class TestViews(TestBase):
 
-    def test_index_view(self):
-        """
-        Test that index is accessible without login
-        """
-        response = self.client.get(url_for('api.index'))
-        self.assertEqual(response.status_code, 200)
-
     def test_login_view(self):
         """
         Test for login
@@ -38,29 +31,6 @@ class TestViews(TestBase):
         response = self.client.get(url_for('api.register'))
         self.assertEqual(response.status_code, 405)
 
-
-
-class TestErrorPages(TestBase):
-
-    def test_403_forbidden(self):
-        # create route to abort the request with the 403 Error
-        @self.app.route('/403')
-        def forbidden_error():
-            abort(403)
-
-        response = self.client.get('/403')
-        self.assertEqual(response.status_code, 403)
-        self.assertTrue("You do not have sufficient permissions to access this resources." in response.data)
-
-    def test_500_internal_server_error(self):
-        # create route to abort the request with the 500 Error
-        @self.app.route('/500')
-        def internal_server_error():
-            abort(500)
-
-        response = self.client.get('/500')
-        self.assertEqual(response.status_code, 500)
-        self.assertTrue("The server encountered an internal error." in response.data)
 
 if __name__ == '__main__':
     unittest.main()
