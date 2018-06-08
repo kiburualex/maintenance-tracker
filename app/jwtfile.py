c5025f70454729c21d6c91b9c984738d8c4ddef4
desshub <desshub95@gmail.com>
2018-06-08 10:15:34 +0300

    [Chore #158208249] Create Logout Endpoint

--- app/jwtfile.py
+++ app/jwtfile.py
@@ -2,6 +2,7 @@ import jwt
 import os
 from flask import current_app
 from datetime import datetime, timedelta
+from connect import conn
 
 
 class Jwt_details(object):
@@ -41,3 +42,33 @@ class Jwt_details(object):
         except jwt.InvalidTokenError:
             # the token is invalid, return an error message
             return "Please register or login"
+
+    @staticmethod 
+    def blacklist_token(token):
+        """Blacklist tokens"""
+        try:
+            blacklisted_on = datetime.now()
+            cur = conn.cursor()
+            cur.execute("INSERT INTO blacklist_tokens(token, blacklisted_on)\
+            VALUES (%s, %s) RETURNING id;",
+                        (token, blacklisted_on))
+            conn.commit()
+            return True
+        except Exception as e:
+            return e
+
+    @staticmethod
+    def is_blacklisted(token):
+        """Check if is blacklisted """
+        try:
+            cur = conn.cursor()
+            cur.execute(
+                "SELECT * FROM blacklist_tokens WHERE token = %s;",
+                (token,))
+            items = cur.fetchone()
+            if items:
+               return True
+            else: 
+                return False
+        except Exception:
+            return False                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
