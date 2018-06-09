96b5a3d288129faf3d4dccd766c3475727c11dcb
desshub <desshub95@gmail.com>
2018-06-09 14:03:35 +0300

    [Chore #158215122] Update tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -3,11 +3,12 @@ import unittest
 import os
 
 from flask import abort, url_for
-from app.user import User_details
+from app.models import User
 from flask_testing import TestCase
 from app import create_app
 from passlib.hash import sha256_crypt
 from connect import conn
+from migrate import create_users
 
 class TestBase(TestCase):
 
@@ -25,16 +26,8 @@ class UserTests(TestBase):
 
     def setUp(self):
         """ Set up user object before each test"""
-        self.user = User_details()
-        cur = conn.cursor()
-        cur.execute("DROP TABLE IF EXISTS users")
-        cur.execute("DROP TABLE IF EXISTS requests")        
-        cur.execute("CREATE TABLE users(id serial PRIMARY KEY, email varchar,\
-         username varchar, role varchar, password varchar);")
-        cur.execute("CREATE TABLE requests(id serial PRIMARY KEY, user_id integer, \
-        category varchar, location varchar, req_date date, req_time time, description varchar, \
-        status varchar, isresolved boolean);")
-        conn.commit()
+        self.user = User()
+        create_users()
 
     def tearDown(self):
         """ Clear up objects after every test"""
@@ -42,39 +35,40 @@ class UserTests(TestBase):
 
     def test_isuccessful_registration(self):
         """Test is a user with correct credentials can register sucessfully"""
-        res = self.user.register("desmond", "desmond@mail.com", "pass1234", "pass1234")
-        self.assertEqual(res, "Registration successfull")
+        res = User("desmond", "desmond@mail.com", "pass1234")
+        user = res.add()
+        self.assertEqual(user["username"], "desmond")
     
     def test_existing_user(self):
         """Test with an already existing user, try registering a user twice"""
-        self.user.register("desmond", "desmond@mail.com", "pass1234", "pass1234")
-        res = self.user.username_exist("desmond")
-        self.assertEqual(res, True)
+        res = User("desmond", "desmond@mail.com", "pass1234")
+        res.add()
+        res2 = User("desmond", "desmond@mail.com", "pass1234")
+        user = res2.add()
+        self.assertEqual(user, "Username Is already taken")
+    
+    def test_view_all_users(self):
+        """Test view all users"""
+        res = User("desmond", "desmond@mail.com", "pass1234")
+        res.add()
+        res2 = User("desmond", "desmond@mail.com", "pass1234")
+        user = res2.add()
+        count = len(self.user.fetch_all())
+        self.assertEqual(count, 2)
 
-    def test_password_length(self):
-        """Test to ensure that a user has a strong password"""
-        res = self.user.register("desmond", "desmond@mail.com", "pass", "pass")
-        self.assertEqual(res, "Password too short")
-
-    def test_password_match(self):
+    def test_verify_password_match(self):
         """Test if password matching is working"""
-        res = self.user.register("desmond", "desmond@mail.com", "pass1234", "patyt1233")
-        self.assertEqual(res, "passwords do not match")
+        res = User("desmond", "desmond@mail.com", "pass1234")
+        user = res.add()
+        h_pass = self.user.verify_password("pass1234", user['password'])
+        self.assertTrue(h_pass, True)
 
     def test_invalid_password(self):
         """Test if password is valid"""
-        res = self.user.login("dessmond", "")
-        self.assertEqual(res, "Password Not Valid")
-
-    def test_invalid_username_register(self):
-        """Test if username is valid"""
-        res = self.user.register("", "desmond@mail.com", "pass1234", "pass1234")
-        self.assertEqual(res, "Username Not Valid")
-
-    def test_invalid_username_login(self):
-        """Test if username is valid"""
-        res = self.user.login("", "pass1234")
-        self.assertEqual(res, "Username Not Valid")
+        res = User("desmond", "desmond@mail.com", "pass1234")
+        user = res.add()
+        h_pass = self.user.verify_password("pass123", user['password'])
+        self.assertFalse(h_pass, False)
 
 
 
