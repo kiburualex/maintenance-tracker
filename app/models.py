c5870a4056ccbf74f50e3f3b6b65a09d5b62f7f2
desshub <desshub95@gmail.com>
2018-07-18 12:55:26 +0300

    UPdate models

--- app/models.py
+++ app/models.py
@@ -192,7 +192,7 @@ class Service(object):
     def ifExist(self, category, location, description, user_id):
         status = "Pending"
         cur.execute(
-            "SELECT * FROM requests WHERE user_id=%s AND category=%s AND location=%s AND status=%s", (user_id, category, location, status))
+            "SELECT * FROM requests WHERE user_id=%s AND description=%s AND status=%s", (user_id, category, location, status))
         requests_tuple = cur.fetchall()
         if requests_tuple:
             return True
